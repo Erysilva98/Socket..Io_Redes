9fb8fc1a9972d19bf9b409dbda86a2de338c6764
Erimilson Silva <eesr@discente.ifpe.edu.br>
2024-08-20 22:01:23 -0300

    ajuste do ip no socket

--- chat-app/src/socket.tsx
+++ chat-app/src/socket.tsx
@@ -1,6 +1,8 @@
 import { io, Socket } from 'socket.io-client';
 
-const SERVER_URL = 'http://10.26.12.100:8000';  // Deve ser o Ip da máquina que está rodando o servidor + port 8000
+const ip = '10.26.12.100'  // Deve ser o Ip da máquina 
+
+const SERVER_URL = `http://${ip}:8000`;  // Deve ser o Ip da máquina que está rodando o servidor + port 8000
 
 const socket: Socket = io(SERVER_URL);
 
