4cee1f79a76be8a769428200cf13177bb8001838
Erimilson Silva <eesr@discente.ifpe.edu.br>
2024-08-23 00:58:03 -0300

    configirar ip do cell

--- chat-app/src/socket.tsx
+++ chat-app/src/socket.tsx
@@ -1,6 +1,6 @@
 import { io, Socket } from 'socket.io-client';
 
-const ip = '10.26.12.100'  // Deve ser o Ip da máquina 
+const ip = '192.168.12.86'  // Deve ser o Ip da máquina 
 
 const SERVER_URL = `http://${ip}:8000`;  // Deve ser o Ip da máquina que está rodando o servidor + port 8000
 
